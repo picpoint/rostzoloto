e6040663e4839727b470c07d7c457a8285107995
rmtar <picpoint@yandex.ru>
2021-10-20 23:52:03 +0300

    work with header homepage

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -6,6 +6,7 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="public/assets/users/css/style.css">
+    <link rel="stylesheet" href="public/assets/users/css/all.min.css">
     <title>{{ $title }}</title>
 </head>
 <body>
@@ -14,13 +15,15 @@
         <div class="homepage__header">
             <div class="homepage__hdrblock">
                 <div class="homepage__post">
-
+                    <i class="far fa-envelope"></i>
+                    <span>1@rostzoloto23.ru</span>
                 </div>
                 <div class="homepage__logo">
-
+                    <img src="public/assets/users/img/logo.png" alt="logo">
                 </div>
                 <div class="homepage__phone">
-
+                    <i class="fas fa-phone-alt"></i>
+                    <span>+7(988)168-43-62</span>
                 </div>
             </div>
         </div>
