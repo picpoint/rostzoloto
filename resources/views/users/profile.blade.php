962a06ecb77f2ea904d28688ee96a9a5719bf355
picpoint <picpoint@yandex.ru>
2021-12-26 13:59:21 +0300

    made show personal page

--- resources/views/users/profile.blade.php
+++ resources/views/users/profile.blade.php
@@ -1,1 +1,12 @@
-<h3>ПРОФИЛЬ {{ \Illuminate\Support\Facades\Auth::user()->firstname }}</h3>
+<div class="personal__personaldata">
+    <div class="personal__headerpersonal">
+        <span>Личный кабинет</span>
+    </div>
+    <div class="personal__alldatas">
+        <h3>ПРОФИЛЬ {{ \Illuminate\Support\Facades\Auth::user()->firstname }}</h3>
+        <a href="{{ route('logout') }}">Выход</a>
+    </div>
+
+
+</div>
+
