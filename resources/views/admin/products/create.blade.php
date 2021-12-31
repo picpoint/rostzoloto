3ead94e913d9260d50bb11e7250889350dac2de3
picpoint <picpoint@yandex.ru>
2021-12-31 09:46:22 +0300

    work with create product

--- resources/views/admin/products/create.blade.php
+++ resources/views/admin/products/create.blade.php
@@ -1,7 +1,105 @@
 @extends('admin.layouts.layout')
 
+
+
 @section('content')
-    <div>
-        <span>Создание товара</span>
+
+    <div class="card card-primary">
+
+
+        @if(session('success'))
+            <div class="alert alert-success">
+                {{ session('success') }}
+            </div>
+        @endif
+
+
+        <div class="card-header">
+            <h3 class="card-title">Создание изделия</h3>
+        </div>
+
+        <form action="{{ route('products.store') }}" method="post" enctype="multipart/form-data">
+            @csrf
+
+            <div class="card-body">
+                <div class="form-group">
+                    <label for="title">Название изделия</label>
+                    <input type="text" name="title" class="form-control" id="title" placeholder="Введите название изделия">
+                </div>
+            </div>
+
+            <div class="form-group" style="width: 96%; margin-left: auto; margin-right: auto">
+                <label for="category_id">Выберите категорию</label>
+                <select class="form-control" id="category_id" name="category_id">
+                    <option>---</option>
+                </select>
+            </div>
+
+            <div class="card-body">
+                <div class="form-group">
+                    <label for="vendor_code">Артикул изделия</label>
+                    <input type="text" name="vendor_code" class="form-control" id="vendor_code" placeholder="Введите артикул изделия">
+                </div>
+            </div>
+
+            <div class="form-group" style="width: 96%; margin-left: auto; margin-right: auto">
+                <label for="material">Выбирите материал изделия</label>
+                <select class="form-control" id="material" name="material">
+                    <option>---</option>
+
+                </select>
+            </div>
+
+            <div class="form-group" style="width: 96%; margin-left: auto; margin-right: auto">
+                <label for="stone">Выбирите вставку(камень)</label>
+                <select class="form-control" id="stone" name="stone">
+                    <option>---</option>
+                </select>
+            </div>
+
+            <div class="card-body">
+                <div class="form-group">
+                    <label for="weight">Введите вес изделия</label>
+                    <input type="text" name="weight" class="form-control" id="weight" placeholder="Вес изделия">
+                </div>
+            </div>
+
+            <div class="card-body">
+                <div class="form-group">
+                    <label for="size">Введите размер изделия</label>
+                    <input type="text" name="size" class="form-control" id="size" placeholder="Размер изделия">
+                </div>
+            </div>
+
+            <div class="card-body">
+                <div class="form-group">
+                    <label for="price">Введите цену изделия</label>
+                    <input type="text" name="price" class="form-control" id="price" placeholder="Цена изделия">
+                </div>
+            </div>
+
+            <div class="card-body">
+                <div class="form-group">
+                    <label for="brand">Бренд</label>
+                    <input type="text" name="brand" class="form-control" id="brand" placeholder="Бренд изделия">
+                </div>
+            </div>
+
+            <div class="form-group" style="width: 96%; margin-left: auto; margin-right: auto">
+                <label for="picture">Загрузить изображение</label>
+                <div class="input-group">
+                    <div class="custom-file">
+                        <input type="file" class="form-control-file" name="picture" id="picture">
+                        <label class="custom-file-label" for="picture">Выберите файл</label>
+                    </div>
+                </div>
+            </div>
+
+
+            <div class="card-footer">
+                <button type="submit" class="btn btn-primary">Создать</button>
+            </div>
+        </form>
     </div>
+
 @endsection
--- resources/views/admin/products/index.blade.php
+++ resources/views/admin/products/index.blade.php
@@ -1,5 +1,35 @@
 @extends('admin.layouts.layout')
 
 @section('content')
-    <span>Страница изделий</span>
+
+    @if(session('success'))
+        <div class="alert alert-success">
+            {{ session('success') }}
+        </div>
+    @endif
+
+    <div class="card">
+        <div class="card-header">
+            <h3 class="card-title">Таблица изделий</h3>
+        </div>
+        <div class="card-body">
+            <table class="table table-bordered">
+                <thead>
+                <tr>
+                    <th style="width: 10px">#</th>
+                    <th>Название изделия</th>
+                    <th>Изображение</th>
+                    <th style="width: 120px; text-align: center">Действие</th>
+                </tr>
+                </thead>
+                <tbody>
+
+                </tbody>
+            </table>
+        </div>
+        <!-- /.card-body -->
+        <div class="card-footer clearfix">
+
+        </div>
+    </div>
 @endsection
