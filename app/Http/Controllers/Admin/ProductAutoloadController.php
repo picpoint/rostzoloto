68878be8b3ab7dc9994bbc3bf4253012dd8d6724
rmtar <picpoint@yandex.ru>
2022-01-18 22:41:57 +0300

    work with autoload products

--- app/Http/Controllers/Admin/ProductAutoloadController.php
+++ app/Http/Controllers/Admin/ProductAutoloadController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Models\Product;
 use Illuminate\Http\Request;
 
 class ProductAutoloadController extends Controller
@@ -13,7 +14,39 @@ class ProductAutoloadController extends Controller
     }
 
     public function autoloadFiles(Request $request) {
-        dd($request->all());
+//        dd($request->autoload_file->path());
+
+        $xml = simplexml_load_file($request->autoload_file);
+
+        foreach ($xml as $product) {
+//            dump($product->title);
+//            dump($product->category_id);
+//            dump($product->vendor_code);
+//            dump($product->material_id);
+//            dump($product->stone_id);
+//            dump($product->weight);
+//            dump($product->size);
+//            dump($product->price);
+//            dump($product->picture);
+
+            Product::create([
+                'title' => $product->title,
+                'slug' => $product->title,
+                'category_id' => $product->category_id,
+                'vendor_code' => $product->vendor_code,
+                'material_id' => $product->material_id,
+                'stone_id' => $product->stone_id,
+                'weight' => $product->weight,
+                'size' => $product->size,
+                'price' => $product->price,
+                'picture' => '000',
+            ]);
+
+            session()->flash('success', 'Загрузка завершена');
+
+            return redirect()->route('admin.products.autoload');
+
+        }
     }
 
 }
