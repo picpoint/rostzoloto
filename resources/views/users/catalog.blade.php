b806122ceff123d8d929359e6f078a4a5e648ca7
picpoint <picpoint@yandex.ru>
2022-01-20 08:57:08 +0300

    work with layout catalog

--- resources/views/users/catalog.blade.php
+++ resources/views/users/catalog.blade.php
@@ -11,32 +11,49 @@
 </head>
 <body>
 
-    <section class="catalog">
-        <div class="catalog__headercat">
-            @include('layouts.header')
-        </div>
+<section class="catalog">
+    <div class="catalog__headercat">
+        @include('layouts.header')
+    </div>
 
-        <div class="catalog__contentcat">
-            <div class="catalog__catwrapper">
-                <div class="catalog__searchcat">
-                    <form action="" method="get" name="formsearch" class="catalog__formsearch">
-                        <input type="search" name="inputsearch">
+    <div class="catalog__contentcat">
+        <div class="catalog__catwrapper">
+
+            <div class="catalog__searchcat">
+                <form action="" method="get" name="formsearch" class="catalog__formsearch">
+                    <input type="search" name="inputsearch">
+                </form>
+            </div>
+
+            <div class="catalog__productscat">
+                <div class="catalog__filter">
+
+                    <form action="#" method="get" class="catalog__filterform">
+                        <div class="catalog__filtercategory">
+                            <div class="catalog__filtercategoryhdr">
+                                <span>Категории</span>
+                            </div>
+                            <div class="catalog__filtercategoryitems">
+
+                                <div class="catalog__itemcategory">
+                                    <input type="checkbox" name="itemcat" id="Браслет" value="Браслет">
+                                    <label for="Браслет">Браслет</label>
+                                </div>
+
+                            </div>
+                        </div>
                     </form>
+
                 </div>
-                <div class="catalog__productscat">
-                    <div class="catalog__filter">
+                <div class="catalog__allproducts">
 
-                    </div>
-                    <div class="catalog__allproducts">
-
-                    </div>
                 </div>
             </div>
         </div>
+    </div>
 
 
-
-    </section>
+</section>
 
 </body>
 </html>
